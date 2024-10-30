3b0fe2b3b500875a0fb12cf2bbacb0d3e7154318
mmm6 <shine0897@163.com>
2024-10-30 18:08:27 +0800

    update remix

---
+++ app/routes/posts.new.tsx
@@ -0,0 +1,8 @@
+import { Textarea } from "~/components/ui/textarea";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Form } from "@remix-run/react";
+
+export default function Page() {
+  return <div>New post</div>;
+}
